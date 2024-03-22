ecfe13d37c19437e71bcc16d33daca51689863e5
jan_karas <97399542+jkaras2008@users.noreply.github.com>
2024-03-22 15:29:11 +0100

    new subnets with errors

--- subnets.cpp
+++ subnets.cpp
@@ -2,6 +2,7 @@
 #include <cmath>
 #include <string>
 #include <algorithm>
+#include <stdlib.h>
 
 using namespace std;
 
@@ -49,11 +50,53 @@ int square(int podsieci) {
 }
 
 void print(string ip, int hosty, int podsieci, int octet) {
-    int help = 0;
-    for (int i = 1; i <= podsieci; i++) {
-        cout << i << ".  " << ip << help+octet << endl;
+    int help = 0, j = 0;
+    for (int i = 1; i <= podsieci-1; i++) {
+        if (help + octet > 255) {
+            cout << "error 4 ";
+            cout << "this ip cannot exist";
+            break;
+        }
+        cout << i << ".  " << ip << help + octet << endl;
         help += hosty;
+        j = i;
     }
+    if (help + octet <= 255) {
+        cout << j + 1 << ".  " << ip << help + octet << endl;
+    }
+}
+bool error(string ip) {
+    int x = ip.length(), temp = 0, still = 15, z = 0;
+    if (x > still) {
+        cout << "error 1 ";
+        return false;
+    }
+    for (int i = 0; i < x; i++) {
+        if (ip[i] == char(46)) {
+            z += 1;
+        }
+    }
+    if (z != 3) {
+        cout << "error 6 ";
+        return false;
+    }
+    int i = 0;
+    string time;
+    for (int j = 0; j < 3; j++) {
+        time.clear();
+        while (ip[i] != char(46)) {
+            time += ip[i];
+            i++;
+        }
+        i++;
+        temp = stoi(time);
+        if (temp > 255) {
+            cout << "error 6 ";
+            return false;
+        }
+    }
+    
+    return true;
 }
 
 int main() {
@@ -61,9 +104,18 @@ int main() {
     int prefiks, podsieci, hosty, octet;
     cout << "type in ip v4: ";
     cin >> ip;
-    cout << "prefix is 24"<<endl;
+    if (error(ip) == false) {
+        cout << "this ip cannot exist";
+        return 3;
+    }
+    cout << "prefix is 24" << endl;
     cout << "type wanted amount of subnets: ";
     cin >> podsieci;
+    if (podsieci > 255) {
+        cout << "error 5 ";
+        cout << "too many subnets";
+        return 3;
+    }
     octet = last_octet(ip);
     ip = no_last_octet(ip);
     hosty = square(podsieci);
